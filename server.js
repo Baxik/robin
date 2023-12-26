1e9ff0182f3579db291b1542d6cd2b1f3febbb3d
Max <voronin223@gmail.com>
2023-12-26 16:01:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,42 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-// Папка для хранения загруженных фотографий
-const uploadFolder = './uploads/';
-app.use(express.static(uploadFolder));
+app.use(express.static('public'));
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadFolder);
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  }
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
 });
 
 const upload = multer({ storage: storage });
 
 app.post('/upload', upload.single('photo'), (req, res) => {
-  res.send('Фотография успешно загружена.');
+    res.send('File uploaded!');
+});
+
+// Обработка GET-запроса для получения списка фотографий
+app.get('/photos', (req, res) => {
+    const uploadPath = path.join(__dirname, 'public/uploads/');
+
+    fs.readdir(uploadPath, (err, files) => {
+        if (err) {
+            console.error('Error reading upload directory:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            // Отправляем список файлов клиенту
+            res.json({ photos: files });
+        }
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
